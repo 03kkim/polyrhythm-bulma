b6ff17bb5288a2f17c03973d31d019e63f6fd40c
03kkm <theq614@gmail.com>
2022-01-06 17:18:23 -0500

    fix rerendering issue but color doesn't change

--- src/App.js
+++ src/App.js
@@ -6,28 +6,18 @@ import * as Tone from 'tone';
 import { useState, useEffect } from 'react';
 import { Button } from './FlashingButton';
 function App() {
-  const [buttonIsOn, setButtonIsOn] = useState(false);
   const [buttonColor, setButtonColor] = useState("");
   let synth = new Tone.Synth().toDestination();
 
-  // document.querySelector('button')?.addEventListener('click', async () => {
-  //   // await Tone.start();
-  //   const d = new Date();
-  //   let time = d.getTime();
-  //   // console.log('audio is ready: ' + time);
-  //   console.log('audio is ready');
-  // });
-  // document.querySelector('button')?.addEventListener('click', async () => {
-  //   await Tone.start();
-  //   console.log('audio is ready');
-  // });
-  // document.querySelector('button').addEventListener('click', () => console.log('hi'));
-  // async function startTone() {
-  //   await Tone.start();
-  //   console.log('audio is ready');
-  // }
+  const synthA = new Tone.FMSynth().toDestination();
+  function attackAndChangeColor(time) {
+    synthA.triggerAttackRelease("C2", "16n", time);
+    changeButtonColor();
+  }
+
   async function startTone() {
     await Tone.start();
+    Tone.Transport.start();
     console.log('audio is ready');
   }
   const changeButtonColor = () => {
@@ -38,25 +28,13 @@ function App() {
       //synth.triggerAttackRelease("C4", "32n");
     }
   }
-  const switchButton = () => {
-    setButtonIsOn(buttonIsOn === false ? true : false);
-  }
-  // const loopA = new Tone.Loop(time => {
-  //   changeButtonColor()}, "4n").start(0);
-  // Tone.Transport.start();
-  
-    // useEffect(() => {
-    //   Tone.Transport.start();
-    //   return () => {
-        
-    //   }
-    // })
-  
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     changeButtonColor()}, 1000);
-  //     return () => clearInterval(interval);
-  // });
+
+  useEffect (() => {
+    const loopA = new Tone.Loop(time => {
+      attackAndChangeColor(time);
+    }, "1n").start(0);
+  }, []);
+
   return (
     <div>
       <div className="box has-text-centered">
--- todos.md
+++ todos.md
@@ -1,2 +1,9 @@
 # Todos
 - Implement tempo slider
+
+
+# Issues
+- Can't seem to use the tone transport to sync visual changes (breaks the web audio)
+  - Fixed it! On Github issues thread
+- Need to be able to execute the loop such that the color changes with the loop without needing to rerender
+
