33fd6f695dc5517e7d093d840022531b5731d62b
03kkm <theq614@gmail.com>
2022-01-10 20:56:03 -0500

    add inputs for polyrhythm

--- src/App.js
+++ src/App.js
@@ -10,18 +10,25 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false); 
   const [tempo, setTempo] = useState(60);
 
-  const synthA = new Tone.FMSynth().toDestination();
+  const synthA = new Tone.MembraneSynth().toDestination();
+  const synthB = new Tone.MembraneSynth().toDestination();
   const sliderValueChanged = useCallback(val => {
     console.log("NEW VALUE", val);
     setTempo(val);
   });
 
-  const clickLoop = useRef(null);
+  const [rhythmA, setRhythmA] = useState("1n");
+  const [rhythmB, setrhythmB] = useState("1n");
+
+
+  const clickLoopA = useRef(null);
+  const clickLoopB = useRef(null);
+
   // keeps track of current beat 
   const currentBeat = useRef(0);
 
-  const subdivisionLength = "4n";
-  const attackReleaseLength = parseInt(subdivisionLength.replace('n', '') * 2).toString() + "n";
+  const subdivisionLengthA = "4n";
+  const attackReleaseLengthA = parseInt(subdivisionLengthA.replace('n', '') * 2).toString() + "n";
   /**
    * CREDIT TO RENZO (renzol2) FOR FIGURING THIS OUT
    * Changes button color based on time:
@@ -39,7 +46,7 @@ function App() {
     
   }
   const updateTempo = () => {
-    Tone.Transport.bpm.rampTo(tempo, 1);
+    Tone.Transport.bpm.rampTo(tempo, 0.1);
   };
   useEffect(() => {
     updateTempo()
@@ -48,23 +55,31 @@ function App() {
   const startTone = () => {
     if (isPlaying) {
       setIsPlaying(false);
-      clickLoop.current.dispose();
+      clickLoopA.current.dispose();
+      clickLoopB.current.dispose();
       Tone.Transport.cancel();
       console.log("stop audio");
     } else {
       setIsPlaying(true);
       // The loop is initialized in clickLoop, which is a useRef() which means that it persists through renders
-      clickLoop.current = new Tone.Loop((time) => {
-        synthA.triggerAttackRelease("C2", attackReleaseLength, time);
+      clickLoopA.current = new Tone.Loop((time) => {
+        synthA.triggerAttackRelease("C1", attackReleaseLengthA, time);
         console.log(time);
-        Tone.Draw.schedule(() => {
-          currentBeat.current = currentBeat.current + 1;
-          // changeButtonColor();
-        }, time)
+        currentBeat.current = currentBeat.current + 1;
+
         
-      }, subdivisionLength);
-      clickLoop.current.start("+0.1");
-      Tone.Transport.start("+0.1");
+      }, subdivisionLengthA);
+
+      clickLoopB.current = new Tone.Loop((time) => {
+        synthA.triggerAttackRelease("C2", attackReleaseLengthA, time);
+        console.log(time);
+        currentBeat.current = currentBeat.current + 1;
+        
+      }, "5n");
+
+      clickLoopA.current.start();
+      clickLoopB.current.start();
+      Tone.Transport.start();
       // sets Transport's BPM
       Tone.Transport.bpm.value = tempo;
       console.log("audio is ready");
@@ -91,6 +106,17 @@ function App() {
       <div className="box">
         <Button color={buttonColor} onClick={startTone} isPlaying={isPlaying} spacing="mb-3" />
         <RangeSlider {...sliderProps} min={33} max={300} step="0.5" />
+      </div>
+      <div className="box">
+        <div className="columns">
+          <div className="column">
+          <input className="input" type="text" placeholder="Note 1" />
+          </div>
+          <div className="column">
+          <input className="input" type="text" placeholder="Note 2" />
+          </div>
+        </div>
+        <Button />
       </div>
     </div>
     <footer className="footer">
